31645d1ebb59ac0eb5637a6b910e715b3b1cc8ea
upskillbootcamps00123 <upskillbootcamps@gmail.com>
2025-03-02 14:27:11 +0430

    we created some pages

---
+++ script.js
@@ -0,0 +1,13 @@
+// function sum() {
+//   console.log("this is body of sum fun");
+// }
+
+// sum();
+
+let add = (num1, num2 = 90) => {
+  console.log(num1 + num2);
+};
+
+add(20);
+
+console.log(20 < 30 ? "hi" : "sorry");
